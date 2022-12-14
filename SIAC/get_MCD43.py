11810add7d4bf021e3fc209834dbd418b979faea
Frédéric Trastour <114990363+ftrastourlux@users.noreply.github.com>
2022-12-14 16:30:58 +0100

    Update get_MCD43.py
    Add debug messages.

--- SIAC/get_MCD43.py
+++ SIAC/get_MCD43.py
@@ -26,6 +26,7 @@ def get_auth(logger):
     logger.info('get_auth')
     try:
         auth = tuple([os.environ['Earthdata_user'], os.environ['Earthdata_pass']])
+        logger.info(f'get_auth: using Earthdata_user={auth[0]} and Earthdata_pass={auth[1]} from env')
         with requests.Session() as s:                                            
             s.auth = auth                                                        
             r1     = s.get(test_url)                                             
@@ -161,6 +162,7 @@ def get_one_tile(tile_date):
     base = 'https://e4ftl01.cr.usgs.gov/MOTA/MCD43A1.006/'
     tile, date = tile_date
     for j in range(100):
+        logger.info(f'get_one_tile: requesting {base + date}')
         r = requests.get(base + date)
         fname = [i.split('>')[-1] for i in r.content.decode().split('</') if (i[-3:]=='hdf') & (tile in i)]
         if len(fname) == 1:
@@ -176,7 +178,9 @@ def downloader(url_fname, auth):
     with requests.Session() as s:
         s.max_redirects = 100000
         s.auth = auth
+        logger.info( f'downloader: getting {url} (auth={auth[0]}/{auth[1]})' )
         r1     = s.get(url)
+        logger.info( f'downloader: getting {r1.url} (auth={auth[0]}/{auth[1]})' )
         r      = s.get(r1.url, stream=True, headers={'user-agent': 'My app'})
         if r.ok:
             remote_size = int(r.headers['Content-Length'])
